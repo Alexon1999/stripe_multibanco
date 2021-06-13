54a95f357fdfae614d474030511f8ba38fb43eef
Alexon1999 <alexon1999@gmail.com>
2021-06-14 01:00:42 +0200

    source chargeable

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const stripe = require("stripe")(
+  "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"
+);
 
 const app = express();
 
@@ -8,10 +11,21 @@ app.get("/", (req, res) => {
   res.json({ msg: "stripe webhook :)" });
 });
 
-app.post("/webhook", (req, res) => {
-  const data = req.body;
+app.post("/webhook", async (req, res) => {
+  // we get the source object
+  const source = req.body;
 
-  res.json({ data });
+  switch (source.type) {
+    case "source.chargeable":
+      const charge = await stripe.charges.create({
+        amount: source.data.object.amount,
+        currency: "eur",
+        source: source.data.object.id,
+      });
+      res.json({ msg: "charge created" });
+    default:
+      res.json(source);
+  }
 });
 
 const PORT = process.env.PORT || 5000;
